f4c94c9f28fdd107f7d3175f3be21947a3bfea4c
thriloknath7989-web <thriloknath7989@gmail.com>
2026-08-29 19:55:40 +0530

    Fix import path for firebase in login page

--- app/login/page.js
+++ app/login/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { auth } from "@/lib/firebase";
+import { auth } from "../../lib/firebase";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 export default function LoginPage() {
